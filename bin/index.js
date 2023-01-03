4a0070023bfcf24b2075deb99e7a1de2a28cfe8e
Aamir Maniar <aamironline@gmail.com>
2023-01-03 23:31:14 +0530

    Added esbuild bundling support

--- bin/index.js
+++ bin/index.js
@@ -1,41 +1,65 @@
-import $l7i9T$puppeteer from "puppeteer";
-import {normalize as $l7i9T$normalize} from "path";
+// src/printeer.js
+import puppeteer from "puppeteer";
+import { normalize } from "path";
+var printeer_default = (url, outputFile, outputType = null) => {
+  return new Promise(async (resolve, reject) => {
+    outputFile = normalize(outputFile);
+    if (!url.startsWith("http")) {
+      reject("URL must start with http or https");
+    }
+    const browser = await puppeteer.launch({ headless: true });
+    const page = await browser.newPage();
+    const res = await page.goto(url, { waitUntil: "networkidle0" });
+    if (!res) {
+      return reject(new Error("Could not load the page."));
+    }
+    outputType = detectOutputType(outputFile, outputType);
+    if (res.status() !== 200) {
+      reject(`Error: ${res.status()}: ${res.statusText()}`);
+    } else {
+      if (outputType === "png") {
+        await page.screenshot({ path: outputFile });
+      } else {
+        await page.pdf({ format: "A4", path: outputFile });
+      }
+      resolve(outputFile);
+    }
+    return await browser.close();
+  });
+};
+function detectOutputType(fname, outputType) {
+  const validOutputTypes = ["pdf", "png"];
+  if (!outputType) {
+    const ext = fname.split(".").pop();
+    if (validOutputTypes.includes(ext)) {
+      return ext;
+    }
+    return "pdf";
+  }
+  if (!validOutputTypes.includes(outputType)) {
+    return "pdf";
+  }
+  return outputType;
+}
 
-
-
-var // networkidle0 - consider navigation to be finished when there are no more than 0 network connections for at least 500 ms
-// networkidle2 - consider navigation to be finished when there are no more than 2 network connections for at least 500 ms.
-/**
- * Generate the report.
- * @param {string} reportName The name of the report.
- * @returns The promise of the report file.
- */ $173d6c6c8da44cf1$export$2e2bcd8739ae039 = (url, outputFile, outputType = "pdf")=>{
-    return new Promise(async (resolve, reject)=>{
-        outputFile = (0, $l7i9T$normalize)(outputFile);
-        if (!url.startsWith("http")) throw new Error("URL must start with http or https");
-        const browser = await (0, $l7i9T$puppeteer).launch({
-            headless: true
-        });
-        const page = await browser.newPage();
-        const res = await page.goto(url, {
-            waitUntil: "networkidle0"
-        });
-        if (!res) return reject(new Error("Could not load the page."));
-        if (res.status() !== 200) reject(`Error: ${res.status()}: ${res.statusText()}`);
-        else {
-            await page.pdf({
-                format: "A4",
-                path: outputFile
-            });
-            resolve(outputFile);
-        }
-        return await browser.close();
-    });
+// src/usage.js
+var usage_default = () => {
+  console.log("Usage: printeer <url> <outputFile>");
 };
 
-
-const $cf838c15c8b009ba$export$4f653a8d4fdb37ed = (0, $173d6c6c8da44cf1$export$2e2bcd8739ae039);
-
-
-export {$cf838c15c8b009ba$export$4f653a8d4fdb37ed as printeer};
+// src/cli.js
+(async function main() {
+  const url = process.argv[2];
+  const outputFile = process.argv[3];
+  if (!url || !outputFile) {
+    usage_default();
+    process.exit(1);
+  }
+  try {
+    await printeer_default(url, outputFile);
+  } catch (e) {
+    console.error("Error:", e);
+    process.exit(1);
+  }
+})();
 //# sourceMappingURL=index.js.map
