c449bb66be377bde51fe17d47b9c370b41e59174
Aamir Maniar <aamironline@gmail.com>
2023-01-19 17:49:22 +0530

    Check if user is a root - WIP

--- src/utils.ts
+++ src/utils.ts
@@ -1,9 +1,13 @@
-
+import * as os from 'os';
 
 // Checks if the current user is root.
 export const isCurrentUserRoot = function():Boolean {
   if (process && process.getuid) {
-    return process.getuid()  == 0; // UID 0 is always root
+    return process.getuid() === 0; // UID 0 is always root
+  }
+
+  if (os.userInfo().username == 'root') {
+    return true;
   }
 
   return false;
